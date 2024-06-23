a33144000561f89d99eff980b8241b83bdf34cad
Giovana Cassias <giovanacassias@outlook.com>
2024-06-23 20:30:07 -0300

    fetch rodando certinho

--- PROJETO-GIOVANA/app/service/user.service.js
+++ PROJETO-GIOVANA/app/service/user.service.js
@@ -2,24 +2,24 @@ export class UserService {
   constructor() {}
 
   // Constante para a chave do armazenamento local
-  LOCAL_STORAGE_KEY = "users";
+   LOCAL_STORAGE_KEY = "users";
 
-  saveLocal(user) {
-    // Obtendo USUARIOS do armazenamento local
-    let users = localStorage.getItem(this.LOCAL_STORAGE_KEY);
-
-    // Verificando se há usuarios existentes
-    users = users ? JSON.parse(users) : [];
-
-    // Adicionando o novo usuário ao array
-    users.push(user);
-
-    // Salvando os usuários atualizados no armazenamento local
-    localStorage.setItem(this.LOCAL_STORAGE_KEY, JSON.stringify(users));
-
-    // Retornando os relatórios atualizados
-    return true;
-  }
+   saveLocal(user) {
+     // Obtendo USUARIOS do armazenamento local
+     let users = localStorage.getItem(this.LOCAL_STORAGE_KEY);
+ 
+     // Verificando se há usuarios existentes
+     users = users ? JSON.parse(users) : [];
+ 
+     // Adicionando o novo usuário ao array
+     users.push(user);
+ 
+     // Salvando os usuários atualizados no armazenamento local
+     localStorage.setItem(this.LOCAL_STORAGE_KEY, JSON.stringify(users));
+ 
+     // Retornando os relatórios atualizados
+     return true;
+   }
 
   //listUsers() busca uma lista dos endereços cadastrados
   async listUsers() {
